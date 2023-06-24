e4f769daa9d2cd7a4e0aa630f934d0aa8016915b
EdwardRodriguezPeacedev <edward.rodriguez@peacedev.co>
2023-06-24 14:56:00 -0500

    swiper

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+  static String name = "homeScreen";
 
   @override
   Widget build(BuildContext context) {
