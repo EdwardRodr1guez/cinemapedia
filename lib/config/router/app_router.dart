53ea7beca95f897947ad761f39438e429387b6b8
EdwardRodriguezPeacedev <edward.rodriguez@peacedev.co>
2023-06-29 15:58:35 -0500

    shell routes

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,12 +1,43 @@
 import 'package:cinemapedia/presentation/screens/movies/home_screen.dart';
 import 'package:cinemapedia/presentation/screens/movies/movie_screen.dart';
+import 'package:cinemapedia/presentation/views/movies/home_tabs/favorite_view.dart';
+import 'package:cinemapedia/presentation/views/movies/home_tabs/home_view.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(initialLocation: "/", routes: [
+  ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      routes: [
+        GoRoute(
+            path: "/",
+            builder: (context, state) {
+              return const HomeView();
+            },
+            routes: [
+              GoRoute(
+                path: "movie/:id", //el id se manda
+                name: MovieScreen.name,
+                builder: (context, state) {
+                  final movieId = state.pathParameters['id'] ?? "no-id";
+                  return MovieScreen(movieId: movieId);
+                },
+              )
+            ]),
+        GoRoute(
+          path: "/favorites",
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        )
+      ])
+
+  /*Rutas padre/hijo
   GoRoute(
     path: "/",
     name: HomeScreen.name,
-    builder: (context, state) => const HomeScreen(),
+    builder: (context, state) => const HomeScreen(childView: FavoritesView()),
   ),
   GoRoute(
     path: "/movie/:id", //el id se manda
@@ -15,5 +46,5 @@ final appRouter = GoRouter(initialLocation: "/", routes: [
       final movieId = state.pathParameters['id'] ?? "no-id";
       return MovieScreen(movieId: movieId);
     },
-  )
+  )*/
 ]);
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,110 +1,15 @@
-import 'package:cinemapedia/presentation/providers/movies/initial_loading_provider.dart';
-import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
-import 'package:cinemapedia/presentation/providers/movies/movies_slideshow_provider.dart';
-import 'package:cinemapedia/presentation/widgets/custom_appbar.dart';
 import 'package:cinemapedia/presentation/widgets/custom_bottomnavigationbar.dart';
-import 'package:cinemapedia/presentation/widgets/custom_cardslider.dart';
-import 'package:cinemapedia/presentation/widgets/full_screen_loader.dart';
-import 'package:cinemapedia/presentation/widgets/movie_horizontal_listview.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final Widget childView;
+  const HomeScreen({super.key, required this.childView});
   static String name = "homeScreen";
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: _HomeView(), bottomNavigationBar: CustomBottomNavigationBar());
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    /*
-    //final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final nowPlayingMovies = ref.watch(moviesSlideshowProvider);
-    */
-    final initiaLoading = ref.watch(InitialLoadingProvider);
-    if (initiaLoading) {
-      return const FullScreenLoader();
-    }
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final slideShowMovies = ref.watch(moviesSlideshowProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-
-    return CustomScrollView(slivers: [
-      const SliverAppBar(
-        floating: true,
-        flexibleSpace: FlexibleSpaceBar(
-          titlePadding: EdgeInsets.all(0),
-          title: CustomAppbar(),
-        ),
-      ),
-      SliverList(
-          delegate: SliverChildBuilderDelegate((context, index) {
-        return Column(
-          children: [
-            CustomCardSwiper(movies: slideShowMovies),
-            MovieHorizontalListview(
-              movies: nowPlayingMovies,
-              title: "En cines",
-              subtitle: "Lunes 20",
-              loadNextPage: () {
-                ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            MovieHorizontalListview(
-              movies: upcomingMovies,
-              title: "Próximamente",
-              subtitle: "Este mes",
-              loadNextPage: () {
-                ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            MovieHorizontalListview(
-              movies: popularMovies,
-              title: "Populares",
-              subtitle: "",
-              loadNextPage: () {
-                ref.read(popularMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            MovieHorizontalListview(
-              movies: topRatedMovies,
-              title: "Mejor calificadas",
-              subtitle: "De todos los tiempos",
-              loadNextPage: () {
-                ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            const SizedBox(
-              height: 50,
-            )
-          ],
-        );
-      }, childCount: 1))
-    ]);
+    return Scaffold(
+        body: childView,
+        bottomNavigationBar: const CustomBottomNavigationBar());
   }
 }
